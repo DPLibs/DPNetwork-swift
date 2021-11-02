bf037ae94c117512e52c53120e6780e12d344483
Dmitriy Polyakov <dmitriyap11@gmail.com>
2021-11-02 15:22:56 +0300

    Fix DPURLRequestGenerator

--- Sources/DPNetwork/DPURLRequestGenerator.swift
+++ Sources/DPNetwork/DPURLRequestGenerator.swift
@@ -10,7 +10,7 @@ import Foundation
 open class DPURLRequestGenerator {
     
     // MARK: - Init
-    init(
+    public init(
         urlPath: URLPath,
         httpMethod: HTTPMethod,
         headers: [Header]? = nil,
